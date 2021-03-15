1f0fb3461aee832cb3401ea53923397dbf928c57
Noah Stodder <nstod001@wch132-10.cs.ucr.edu>
2021-03-14 21:54:11 -0700

    finished 1.0 of project

--- code/java/src/DBproject.java
+++ code/java/src/DBproject.java
@@ -319,26 +319,177 @@ public class DBproject{
 
 
 	public static void AddCaptain(DBproject esql) {//2
-	}
+	try{
+                System.out.print("\tEnter Captain ID: ");
+                String input_ID = in.readLine();
+                System.out.print("\tEnter the Full Name of the Captain: ");
+                String input_fullname = in.readLine();
+                System.out.print("\tEnter the Nationality of the Captain: ");
+                String input_nationality = in.readLine();
+                String query = "INSERT INTO Captain(id, fullname, nationality) VALUES (" + input_ID + ", '" + input_fullname + "', '" + input_nationality + "')";
+
+                esql.executeUpdate(query);
+                System.out.println("Added Captain");
+                }
+        catch(Exception e){
+                System.err.println(e.getMessage());
+                }
+        }
 
 	public static void AddCruise(DBproject esql) {//3
+	try{
+                System.out.print("\t Enter Cruise number: ");
+                String input_cnum = in.readLine();
+                System.out.print("\t Enter the cost of this Cruise: $");
+                String input_cost = in.readLine();
+                System.out.print("\t Enter the number of tickets initially sold: ");
+                String input_num_sold = in.readLine();
+                System.out.print("\t Enter the number of stops: ");
+                String input_num_stops = in.readLine();
+                System.out.print("\t Enter the actual departure date (YYYY-MM-DD HR:MIN): ");
+                String input_actual_departure_date = in.readLine();
+                System.out.print("\t Enter the actual_arrival_date (YYYY-MM-DD HR:MIN): ");
+                String input_actual_arrival_date = in.readLine();
+                System.out.print("\t Enter the arrival_port: ");
+                String input_arrival_port = in.readLine();
+                System.out.print("\t Enter the departure_port: ");
+                String input_departure_port = in.readLine();
+
+                String query = "INSERT INTO Cruise(cnum, cost, num_sold, num_stops, actual_departure_date, actual_arrival_date, arrival_port, departure_port) VALUES (" + input_cnum + ", " + input_cost + ", " + input_num_sold + ", " + input_num_stops + ", '" + input_actual_departure_date + "', '" + input_actual_arrival_date + "', '" + input_arrival_port + "', '" + input_departure_port +  "')";
+		//NOTE: actual_departure_date may or may not need single quotes.
+                esql.executeUpdate(query);
+                System.out.println("Added Cruise");
+         	}
+         catch(Exception e){
+                System.err.println(e.getMessage());
+                }
 	}
 
 
 	public static void BookCruise(DBproject esql) {//4
+	try{
+		System.out.println("\t Enter Customer information.");
+                System.out.print("\t Enter Customer ID: ");
+                String input_id = in.readLine();
+                System.out.print("\t Enter your first name: ");
+                String input_fname = in.readLine();
+                System.out.print("\t Enter your last name: ");
+                String input_lname = in.readLine();
+                System.out.print("\t Enter your gender(M/F): ");
+                String input_gtype = in.readLine();
+                System.out.print("\t Enter your date of birth date (MM/DD/YYYY): ");
+                String input_dob = in.readLine();
+		System.out.print("\t Enter your address (123 Street St City): ");
+		String input_address = in.readLine();
+                System.out.print("\t Enter your phone number:");
+                String input_phone = in.readLine();
+                System.out.print("\t Enter your zipcode:");
+                String input_zipcode = in.readLine();
+		
+		String query = "INSERT INTO Customer(id,fname,lname,gtype,dob,address,phone,zipcode) VALUES(" + input_id + ", '" + input_fname + "', '" + input_lname + "', '" + input_gtype + "','" + input_dob + "','" + input_address + "', '" + input_phone + "', '" + input_zipcode + "')";
+                esql.executeUpdate(query);
+                System.out.println("Added Customer");
+	
+		System.out.print("\t Enter today’s date:");
+                String today_date = in.readLine();
+		System.out.print("\t Enter the cruise you want(currently cnum):");
+                String input_cnum = in.readLine();
+
+		query = " SELECT C.num_sold, S.seats, C.actual_departure_date FROM Cruise C, CruiseInfo CI, Ship S WHERE C.cnum = " + input_cnum + " AND CI.cruise_id = C.cnum  AND S.id = CI.ship_id";
+		List<List<String>> rs = esql.executeQueryAndReturnResult(query);
+
+		//int today_year = Integer.parseInt(today_date[0])*1000 +Integer.parseInt(today_date[1]) * 100 + Integer.parseInt(today_date[2])*10 + Integer.parseInt(today_date[3]);
+		//int today_month = Integer.parseInt(today_date[5])*10 + Integer.parseInt(today_date[6]);
+		//int today_day = Integer.parseInt(today_date[8])*10 + Integer.parseInt(today_date[9]);
+		int today_year = Integer.parseInt(today_date.substring(0,4));
+		int today_month = Integer.parseInt(today_date.substring(5,7));
+		int today_day = Integer.parseInt(today_date.substring(8,10));
+		//int query_year = Integer.parseInt(rs.get(0).get(2)[0])*1000 + Integer.parseInt(rs.get(0).get(2)[1])*100 + Integer.parseInt(rs.get(0).get(2)[2])*10 + Integer.parseInt(rs.get(0).get(2)[3]);
+		//int query_month = Integer.parseInt(rs.get(0).get(2)[5])*10 + Integer.parseInt(rs.get(0).get(2)[6]);
+		//int query_day = Integer.parseInt(rs.get(0).get(2)[8])*10 + Integer.parseInt(rs.get(0).get(2)[9]);
+		int query_year = Integer.parseInt(rs.get(0).get(2).substring(0,4));
+		int query_month = Integer.parseInt(rs.get(0).get(2).substring(5,7));
+		int query_day = Integer.parseInt(rs.get(0).get(2).substring(8,10));
+		
+		System.out.println("Today's Date: " + today_month + "/" + today_day + "/" + today_year);
+		System.out.println("Query Date: " + query_month + "/" + query_day + "/" + query_year);
+
+		System.out.print("\t Enter Reservation Number: ");
+		String input_rnum = in.readLine();
+		query = "INSERT INTO Reservation(rnum,ccid,cid,status) VALUES(" + input_rnum + ", " + input_id + ", " + input_cnum + ", ";
+
+		if(today_year > query_year || (today_year == query_year && today_month > query_month) || (today_year == query_year && today_month == query_month && today_day >= query_day)){
+			//today’s date is after the cruise has departed
+			query += "'C');";
+		}
+		else if(Integer.parseInt(rs.get(0).get(0)) >= Integer.parseInt(rs.get(0).get(1))){
+			//more sold than seats
+			query += "'W');";
+		}
+		else if(Integer.parseInt(rs.get(0).get(0)) < Integer.parseInt(rs.get(0).get(1))){
+			//less sold than seats
+			query += "'R');";
+		}
+	
+		esql.executeUpdate(query);
+                System.out.println("Added Reservation"); 
+	}//end try
+	catch(Exception e){
+                System.err.println(e.getMessage());
+        }//end catch
+
+
+
 		// Given a customer and a Cruise that he/she wants to book, add a reservation to the DB
 	}
 
 	public static void ListNumberOfAvailableSeats(DBproject esql) {//5
+	try{
+		System.out.print("\t Enter Cruise Number: ");
+		String input_cnum = in.readLine();
+		System.out.print("\t Enter Departure Date (YYYY-MM-DD HR:MIN): ");
+		String input_date = in.readLine();
+
+		List<List<String>> rs1 = esql.executeQueryAndReturnResult("SELECT C.num_sold FROM CruiseInfo CI, Cruise C, Ship S WHERE C.cnum = CI.cruise_id AND S.id = CI.ship_id AND '" + input_date + "'= C.actual_departure_date AND " + input_cnum + " = C.cnum");
+			
+		List<List<String>> rs2 = esql.executeQueryAndReturnResult("SELECT S.seats FROM CruiseInfo CI, Cruise C, Ship S WHERE C.cnum = CI.cruise_id AND S.id = CI.ship_id AND '" + input_date + "'= C.actual_departure_date AND " + input_cnum + " = C.cnum");
+
+		int numsold = Integer.parseInt(rs1.get(0).get(0));
+		int numseats = Integer.parseInt(rs2.get(0).get(0));
+		int result = numseats - numsold;
+		System.out.println("Available Seat(s): " + result);
+		}
+	catch(Exception e){
+		System.err.println(e.getMessage());
+		}
 		// For Cruise number and date, find the number of availalbe seats (i.e. total Ship capacity minus booked seats )
 	}
 
 	public static void ListsTotalNumberOfRepairsPerShip(DBproject esql) {//6
+	try{
+		String query = "SELECT S.id, COUNT(R.rid) FROM Ship S, Repairs R WHERE S.id = R.ship_id GROUP BY S.id ORDER BY COUNT(R.rid) DESC";
+		esql.executeQueryAndPrintResult(query);
+		}//end try
+	catch(Exception e){
+		System.err.println(e.getMessage());
+		}//end catch
+
 		// Count number of repairs per Ships and list them in descending order
 	}
 
 	
 	public static void FindPassengersCountWithStatus(DBproject esql) {//7
+	try{
+		System.out.print("\t Enter Cruise Number: ");
+                String input_cnum = in.readLine();
+		System.out.print("\t Enter Status You Would Like to Search For (W/C/R): ");
+		String input_status = in.readLine();
+		String query = "SELECT COUNT(Cu.id) FROM Cruise C, Customer Cu, Reservation R WHERE C.cnum = R.cid AND R.ccid = Cu.id AND R.status = '" + input_status + "' AND C.cnum =" + input_cnum;
+		esql.executeQueryAndPrintResult(query);			
+		}//end try
+	catch(Exception e){
+                System.err.println(e.getMessage());
+                }//end catch		
 		// Find how many passengers there are with a status (i.e. W,C,R) and list that number.
 	}
 }
